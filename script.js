84c6797848d353a0b42ac2d0c563f8c8e30351f1
WakkaWabba <113367603+WakkaWabba@users.noreply.github.com>
2023-01-11 18:43:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,72 +4,72 @@ function blank() {
    audio.play();
 }
 function zeek() {
-  document.getElementById("character-box").style.backgroundImage = "url('/Images/zeekbig.png')";
+  document.getElementById("character-box").style.backgroundImage = "url('./Images/zeekbig.png')";
    var audio = document.getElementById("zeek-audio");
    audio.play();
 }
 function tony() {
-  document.getElementById("character-box").style.backgroundImage = "url('/Images/tonybig.png')";
+  document.getElementById("character-box").style.backgroundImage = "url('./Images/tonybig.png')";
   var audio = document.getElementById("tony-audio");
   audio.play();
 }
 function amz() {
-  document.getElementById("character-box").style.backgroundImage = "url('/Images/amzbig.png')";
+  document.getElementById("character-box").style.backgroundImage = "url('./Images/amzbig.png')";
   var audio = document.getElementById("amz-audio");
   audio.play();
 }
 function bibz() {
-  document.getElementById("character-box").style.backgroundImage = "url('/Images/bibzbig.png')";
+  document.getElementById("character-box").style.backgroundImage = "url('./Images/bibzbig.png')";
   var audio = document.getElementById("bibz-audio");
   audio.play();
 }
 function aaron() {
-  document.getElementById("character-box").style.backgroundImage = "url('/Images/aaronbig.png')";
+  document.getElementById("character-box").style.backgroundImage = "url('./Images/aaronbig.png')";
   var audio = document.getElementById("aaron-audio");
   audio.play();
 }
 function dillon() {
-  document.getElementById("character-box").style.backgroundImage = "url('/Images/dillonbig.png')";
+  document.getElementById("character-box").style.backgroundImage = "url('./Images/dillonbig.png')";
   var audio = document.getElementById("dillon-audio");
   audio.play();
 }
 function hayden() {
-  document.getElementById("character-box").style.backgroundImage = "url('/Images/haydenbig.png')";
+  document.getElementById("character-box").style.backgroundImage = "url('./Images/haydenbig.png')";
   var audio = document.getElementById("hayden-audio");
   audio.play();
 }
 function nick() {
-  document.getElementById("character-box").style.backgroundImage = "url('/Images/nickbig.png')";
+  document.getElementById("character-box").style.backgroundImage = "url('./Images/nickbig.png')";
   var audio = document.getElementById("nick-audio");
   audio.play();
 }
 function colin() {
-  document.getElementById("character-box").style.backgroundImage = "url('/Images/colinbig.png')";
+  document.getElementById("character-box").style.backgroundImage = "url('./Images/colinbig.png')";
   var audio = document.getElementById("colin-audio");
   audio.play();
 }
 function jess() {
-  document.getElementById("character-box").style.backgroundImage = "url('/Images/jessbig.png')";
+  document.getElementById("character-box").style.backgroundImage = "url('./Images/jessbig.png')";
   var audio = document.getElementById("jess-audio");
   audio.play();
 }
 function rod() {
-  document.getElementById("character-box").style.backgroundImage = "url('/Images/rodbig.png')";
+  document.getElementById("character-box").style.backgroundImage = "url('./Images/rodbig.png')";
   var audio = document.getElementById("rod-audio");
   audio.play();
 }
 function jack() {
-  document.getElementById("character-box").style.backgroundImage = "url('/Images/jackbig.png')";
+  document.getElementById("character-box").style.backgroundImage = "url('./Images/jackbig.png')";
   var audio = document.getElementById("jack-audio");
   audio.play();
 }
 function joe() {
-  document.getElementById("character-box").style.backgroundImage = "url('/Images/joebig.png')";
+  document.getElementById("character-box").style.backgroundImage = "url('./Images/joebig.png')";
   var audio = document.getElementById("joe-audio");
   audio.play();
 }
 function tim() {
-  document.getElementById("character-box").style.backgroundImage = "url('/Images/timbig.png')";
+  document.getElementById("character-box").style.backgroundImage = "url('./Images/timbig.png')";
   var audio = document.getElementById("tim-audio");
   audio.play();
 }
